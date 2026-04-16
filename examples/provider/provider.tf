8c0acf7f53cf8f367e59efe8708ba037fc1cc68f
Miles Bardon <milesbardon@gmail.com>
2026-04-16 18:20:46 +0100

    docs: Generating documentation (#26)
    * docs: Adding documentation for provider and changing customer_id to controller_id
    
    * docs: Adding documentation for sites data source
    
    * docs: Adding documentation for site resource
    
    * docs: Removing scaffolding exmaples
    
    * docs: Adding examples for each resource and data source
    
    * docs: Generating documentation

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,6 @@
-provider "scaffolding" {
-  # example configuration here
+provider "omada" {
+  host          = "http://localhost:8043"
+  controller_id = "example-controller-id"
+  client_id     = "example-client-id"
+  client_secret = "example-client-secret"
 }
---
+++ examples/resources/omada_site/import.sh
@@ -0,0 +1,2 @@
+# Site can be imported by specifying the site ID.
+terraform import omada_site.example 1234567890
